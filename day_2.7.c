5da23b321db942551ba01704ad823d49e76f47ce
Saif12628 <muyeensaifullah7@gmail.com>
2026-05-28 00:04:53 +0600

    Fixed bug in day_2.7.c

--- day_2.7.c
+++ day_2.7.c
@@ -2,20 +2,20 @@
 
 int main(){
 
-    int row, col, total, space = 0;
+    int row, col, total, space;
 
-    printf("Enter the number of rows and columns for the inverted half right triangle pyramid: ");
+    printf("Enter the Number of rows and columns for the inverted half right triangle pyramid: ");
     scanf("%d", &total);
 
-    for(row =1; row <=total; row ++){
-        for(space = 1; space < row; space++){
+    for(row = 1; row <= total; row++){
+        for(space = 2; space <= row; space++){
             printf("  ");
-        }
-        for(col = row; col <= total; col++){
+            }
+        for(col = total; col >= row; col--){
             printf("* ");
         }
         printf("\n");
     }
-    
+
     return 0;
 }
